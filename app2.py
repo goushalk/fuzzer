c2ed1c8297bd3e23429b19b5ad8561c2c8147c37
harish-pushan <harishpushan@gmail.com>
2024-10-03 23:49:02 +0530

    xss  command

--- app2.py
+++ app2.py
@@ -1,22 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import json
+import subprocess
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def json_view():
-    try:
-        # Load the JSON data from the file you uploaded
-        json_file_path = os.path.join(os.getcwd(), 'output.json')
-        with open(json_file_path, 'r') as f:
-            data = json.load(f)
-        
-        # Pass 'data' to the HTML template
-        return render_template('data.html', data=data)
-    
-    except Exception as e:
-        return f"Error loading JSON file: {e}"
+@app.route('/xss', methods=['GET'])
+def xss_form():
+    # Render the form for user input
+    return render_template('index.html')
+
+@app.route('/xss', methods=['POST'])
+def process_xss():
+    url = request.json.get('url')
+    if url:
+        # Sanitize the URL for safety
+        if os.path.exists('xxs.json'):
+            os.remove('xss.json')  
+        # Run the wfuzz command and capture the output using subprocess
+        try:
+            os.system(f"wfuzz -c -z file,xss.txt -f xss.json,json -u {url}?q=FUZZ")
+        except:
+            return "Error "
+    return jsonify({"error": "Invalid request"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
